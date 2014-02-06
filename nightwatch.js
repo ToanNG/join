116ce75bde1ba7ec8db948efd7524afd11774fea
Toan Nguyen <toan.nguyen@2359media.com.vn>
2014-02-06 14:30:41 +0700

    add nightwatch module

---
+++ nightwatch.js
@@ -0,0 +1,3 @@
+require('nightwatch/bin/runner.js');
+//run Selenium server
+//java -jar selenium-server-standalone-2.39.0.jar
---
+++ nightwatch/tests/join.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "Demo test Join" : function (browser) {
+    browser
+      .url("http://localhost:3000")
+      .waitForElementVisible('body', 1000)
+      .setValue('#login-form input[name=username]', 'user1')
+      .setValue('#login-form input[name=password]', 'admin2359')
+      .waitForElementVisible('#login-form #submit-button', 1000)
+      .click('#login-form #submit-button')
+      .pause(1000)
+      .assert.containsText('body', 'Toan Nguyen')
+      .end();
+  }
+};
